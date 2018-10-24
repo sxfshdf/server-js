7275d80db2c8610c452ee19d1ba84cf31b4592bc
sixiaofeng <sxf754872493@gmail.com>
2018-10-25 01:32:19 +0800

    server-js JSONP

---
+++ server.js
@@ -0,0 +1,62 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('方方说：含查询字符串的路径\n' + pathWithQuery)
+
+  if(path === '/'){
+    var string = fs.readFileSync('./index.html','utf8')
+    var amount = fs.readFileSync('./db','utf8') // 读取数据 100
+    string = string.replace('&&&amount&&&',amount) //  替换占位符
+    response.setHeader('Content-Type','text/html;charest=utf-8')
+    response.write(string)
+    response.end()
+  }else if(path === '/style.css'){
+    var string = fs.readFileSync('./style.css','utf8')
+    response.setHeader('Content-Type','text/css')
+    response.write(string)
+    response.end()
+  }else if(path === '/main.js'){
+    var string = fs.readFileSync('./main.js','utf8')
+    response.setHeader('Content-Type','application/javascript')
+    response.write(string)
+    reponse.end()
+  }else if(path === '/pay' && method.toUpperCase()==='POST'){
+    var amount = fs.readFileSync('./db','utf8') // 读取数据是多少 100
+    var newAmount = amount - 1 // 得到新的数据
+    fs.writeFileSync('./db',newAmount) // 写入新的数据
+    response.setHeader('Content-Type','application/javascript')
+    response.statusCode = 200
+    response.write(`${callback}.call(undefined,'success')`)
+    response.end()
+  }else{
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('找不到对应的路径，你需要自行修改 index.js')
+    response.end()
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
+
